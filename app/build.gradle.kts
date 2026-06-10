e3ac42109714110ddd2647bd3458ab3c1d2bfa78
Cristian Moreno <CristianMoreno322@gmail.com>
2026-06-09 22:47:38 -0500

    se corrigue version y se crean primeras vistas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,22 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.construrrenta.app"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.construrrenta.app"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -33,6 +29,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
@@ -41,7 +43,16 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-ktx:1.9.1")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("io.coil-kt:coil:2.7.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
-// Dentro del bloque android {}
-buildFeatures {
-    viewBinding = true
-}
-
-// En el bloque dependencies {}
-// Lifecycle y ViewModel
-implementation(libs.androidx.lifecycle.viewmodel.ktx) // O usa versión directa: "androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4"
-implementation(libs.androidx.lifecycle.runtime.ktx)
-implementation(libs.androidx.activity.ktx)
-
-// Retrofit y Gson (Red)
-implementation("com.squareup.retrofit2:retrofit:2.11.0")
-implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-
-// Corrutinas (Asincronismo)
-implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-
-// Coil (Carga de imágenes para el catálogo)
-implementation("io.coil-kt:coil:2.7.0")
